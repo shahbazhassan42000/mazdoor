0f9f2a3e4db6081d7672b6931afed7bdef6924c0
Shahbaz <shahbazhassan42000@gmail.com>
2022-10-04 10:44:38 +0500

    completed services

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 import logo from '../assets/images/mazdoor-logo-file-web 1.png'
 const Header=()=>{
     return (
-        <header id="top" className="flex w-full justify-between fixed top-0 py-5 px-20">
-            <a href="#top" className="">
+        <header className="z-[999] bg-white flex w-full justify-between fixed top-0 py-5 px-20">
+            <a href="#root" className="">
                 <img src={logo} alt="logo"/>
             </a>
             <div>
                 <ul className="flex font-['poppins'] space-x-10 mt-[20px]">
-                    <i className="not-italic"><a className="actNav text-[18px] hover:text-[#EB5757]" href="#whyMazdoor">Why MAZDOOR</a></i>
-                    <i className="not-italic"><a className="hover:text-[#EB5757]" href="#services">Services</a></i>
-                    <i className="not-italic"><a className="hover:text-[#EB5757]" href="#mazdoors">Mazdoors</a></i>
-                    <i className="not-italic"><a className="hover:text-[#EB5757]" href="#team">Team</a></i>
-                    <i className="not-italic"><a className="hover:text-[#EB5757]" href="#aboutUs">About Us</a></i>
+                    <i className="not-italic"><a className="actNav nav text-[18px] hover:text-[#EB5757] font-[500]" href="#whyMazdoor">Why MAZDOOR</a></i>
+                    <i className="not-italic"><a className="nav hover:text-[#EB5757] font-[500]" href="#services">Services</a></i>
+                    <i className="not-italic"><a className="nav hover:text-[#EB5757] font-[500]" href="#mazdoors">Mazdoors</a></i>
+                    <i className="not-italic"><a className="mav hover:text-[#EB5757] font-[500]" href="#team">Team</a></i>
+                    <i className="not-italic"><a className="nav hover:text-[#EB5757] font-[500]" href="#aboutUs">About Us</a></i>
                     <i className="not-italic"><a className="hover:text-[#EB5757]" href="#search"><span className="fa fa-magnifying-glass"></span></a></i>
                     <i className="not-italic"><a className="bg-[#EB5757] hover:bg-[#F2C94C] text-white px-[20px] py-[14px] rounded-[25px]" href="/login"><span className="fa fa-arrow-right-to-bracket mr-2"></span>Login</a></i>
                 </ul>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,26 @@
 import drillMan from '../../assets/gifs/8t4M.gif'
+import Services from "./Services";
+import {useEffect} from "react";
 const Home = () => {
+    useEffect(()=>{
+        const changeActiveNav=()=>{
+            // console.log(window.pageYOffset)
+            const navs=document.getElementsByClassName("nav");
+            for(let nav of navs){
+                nav.classList.remove("actNav");
+            }
+            if(window.pageYOffset>=0 && window.pageYOffset<541){
+                navs[0].classList.add("actNav");
+            }
+            else if(window.pageYOffset>=541){
+                navs[1].classList.add("actNav");
+            }
+        };
+        window.addEventListener("scroll",changeActiveNav);
+        return ()=>{
+            window.removeEventListener("scroll",changeActiveNav);
+        };
+    },[])
     return (
         <>
             <div id="whyMazdoor" className="flex justify-between px-20 mt-[90px]">
@@ -8,12 +29,12 @@ const Home = () => {
                     <h1 className="font-bold text-[72px] leading-[105px] relative">
                         <span className="absolute bg-[#EB5757] w-[6px] h-[6px] rounded-[50%] -top-3 left-72"></span>
                         Be The Fastest In Getting Mazdoor at
-                        <span className="actNav relative">
+                        <span className="actNav !font-bold relative">
                             <span className="absolute bg-[#F2C94C] w-[10px] h-[10px] rounded-[50%] top-6 -right-36"></span>
                         DoorStep
                         </span>
                     </h1>
-                    <p className="text-[18px] leading-[30px] w-[430px]">
+                    <p className="text-[18px] leading-[30px] w-[430px] font-[500]">
                         Our job is to filling your tummy with delicious food and with fast and free delivery
                     </p>
                     <a className="text-center w-[140px] bg-[#EB5757] hover:bg-[#F2C94C] text-white px-[20px] py-[14px] rounded-[25px]" href="#mazdoors">
@@ -25,7 +46,7 @@ const Home = () => {
                     <span className="absolute rotate-[-25deg] bg-[#EB5757] w-[14px] h-[14px] rounded-[3px] bottom-48 right-9"></span>
                 </div>
             </div>
-
+            <Services/>
         </>
     );
 }
