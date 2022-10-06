430b5e9cbd4348ab09933ce7a7b8af9aedf24ed7
Shahbaz <shahbazhassan42000@gmail.com>
2022-10-06 22:48:50 +0500

    completed mazdoor info

--- src/components/Home/Mazdoor/MazdoorInfo.jsx
+++ src/components/Home/Mazdoor/MazdoorInfo.jsx
@@ -2,32 +2,63 @@ const MazdoorInfo = ({mazdoor, setMazdoorInfo}) => {
 
     return (
         <div
-            className="relative flex flex-col w-[60] bg-[#E0E0E0] w-[60%] mt-[48px] mb-[80px] w-[768px] rounded-[20px] p-[16px] shadow-[10px_10px_4px_0px_#00000080] select-none">
-            <div className="relative flex justify-center items-center h-[60%] ">
-                <div className="w-[85px] h-[85px] border-[2px] border-[#EB5757] rounded-[50%]">
-                    <img className="object-cover h-[100%] w-[100%] rounded-[50%]" src={mazdoor.img}
+            className="relative flex flex-col bg-[#E0E0E0] mt-[48px] mb-[80px] w-[500px] rounded-[20px] p-[20px] shadow-[10px_10px_4px_0px_#00000080] select-none">
+             <span
+                 onClick={() =>setMazdoorInfo(false)}
+                 className="absolute border border-[#EB5757]  top-4 right-4 fa fa-close text-2xl text-[#EB5757] hover:bg-[#091e4214] h-[35px] w-[35px] flex justify-center items-center rounded-[50%]">
+                </span>
+            <div className="flex w-full space-x-[20px] mb-[17px]">
+                <div className=" w-[50%] h-[250px] border-[2px] border-[#EB5757] rounded-[12px]">
+                    <img className="object-cover h-[100%] w-[100%] rounded-[12px]" src={mazdoor.img}
                          alt="face of mazdoor"/>
                 </div>
-                <span
-                    onClick={() =>setMazdoorInfo(false)}
-                    className="absolute border border-[#EB5757]  top-0 right-0 fa fa-close text-2xl text-[#EB5757] hover:text-[#F2C94C] hover:border-[#F2C94C]  h-[40px] w-[40px] flex justify-center items-center rounded-[50%]">
-                </span>
-            </div>
-            <div className="h-[25%]">
-                <h2 className="w-full overflow-hidden truncate font-bold text-[19px] leading-[23px] text-center text-[#222222]">
-                    {mazdoor.name}
-                </h2>
-            </div>
-            <div className="h-[15%]">
-                <div className="flex justify-between">
-                    <span className="text-[#7E7E7E] text-[10px] leading-[8px] font-[600]">Rating</span>
-                    <span className="text-[#7E7E7E] text-[10px] leading-[8px] font-[600]">{mazdoor.rating}%</span>
-                </div>
-                <div className="w-full bg-white rounded-[100px] mt-1 h-[6.65px]">
-                    <div style={{width: `${mazdoor.rating}%`}}
-                         className=" bg-[#EB5757] rounded-[100px] h-[6.65px]">
+                <div className="flex w-[50%] flex-col justify-center items-center space-y-[20px] p-[20px]">
+                    <h2 className="w-full  font-bold text-[24px] leading-[25px] text-center text-[#222222]">
+                        {mazdoor.name}
+                    </h2>
+                    <div className="w-full">
+                        <div className="flex justify-between">
+                            <span className="text-[#7E7E7E] text-[14px] leading-[10px] font-[600]">Rating</span>
+                            <span className="text-[#7E7E7E] text-[14px] leading-[10px] font-[600]">{mazdoor.rating}%</span>
+                        </div>
+                        <div className="w-full bg-white rounded-[100px] mt-1 h-[8.65px]">
+                            <div style={{width: `${mazdoor.rating}%`}}
+                                 className=" bg-[#EB5757] rounded-[100px] h-[8.65px]">
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+
+            </div>
+            <div className="flex flex-col mx-auto space-y-2 mb-[17px]">
+                <div className="flex space-x-2">
+                    <h3 className="font-[700] text-[19px] leading-[22px] text-[#222222] w-[100px]">Age:</h3>
+                    <p className="text-[19px] leading-[22px]">{mazdoor.age}</p>
+                </div>
+                <div className="flex space-x-2">
+                    <h3 className="font-[700] text-[19px] leading-[22px] text-[#222222] w-[100px]">CNIC:</h3>
+                    <p className="text-[19px] leading-[22px]">{mazdoor.cnic==="-"?"-":mazdoor.cnic.slice(0,5)+"-"+mazdoor.cnic.slice(5)}</p>
+                </div>
+                <div className="flex space-x-2">
+                    <h3 className="font-[700] text-[19px] leading-[22px] text-[#222222] w-[100px]">Area:</h3>
+                    <p className="text-[19px] leading-[22px]">{mazdoor.area}</p>
+                </div>
+                <div className="flex space-x-2">
+                    <h3 className="font-[700] text-[19px] leading-[22px] text-[#222222] w-[100px]">Type:</h3>
+                    <p className="text-[19px] leading-[22px]">{mazdoor.type}</p>
+                </div>
+                <div className="flex space-x-2">
+                    <h3 className="font-[700] text-[19px] leading-[22px] text-[#222222] w-[100px]">Contact:</h3>
+                    <p className="text-[19px] leading-[22px]">{mazdoor.phone.slice(0,4)}-{mazdoor.phone.slice(4)}</p>
+                </div>
+
+            </div>
+            <div className="flex justify-center">
+                <a className="text-center w-[100px] bg-[#EB5757] hover:bg-[#F2C94C]  text-white px-[20px] py-[12px] rounded-[25px]"
+                   href="#">
+                    CALL
+                </a>
             </div>
         </div>
     );
