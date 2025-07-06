7883fae24954df59478c86e2d096807f8d661c3d
Shahbaz Hassan <shahbaz.hassan@surveyauto.com>
2025-07-06 15:31:30 +0500

    refactor: Simplify BrowseServices component by removing unused state and actions; update labor actions and slice for better clarity

--- mazdoor_app_v2/store/reducers/index.ts
+++ mazdoor_app_v2/store/reducers/index.ts
@@ -1,22 +1,5 @@
 import LaborReducer, { laborActions } from "@/store/reducers/labor/laborSlice";
 import * as LaborActionCreator from "@/store/reducers/labor/labor.actions";
-import {
-  fetchLaborersWithTypes,
-  selectLaborTypes,
-  selectLaborCategories,
-  selectTransformedLaborers,
-  selectLaborLoading,
-  selectLaborError,
-} from "@/store/reducers/labor/labor.actions";
+import {} from "@/store/reducers/labor/labor.actions";
 
-export {
-  laborActions,
-  LaborReducer,
-  LaborActionCreator,
-  fetchLaborersWithTypes,
-  selectLaborTypes,
-  selectLaborCategories,
-  selectTransformedLaborers,
-  selectLaborLoading,
-  selectLaborError,
-};
+export { laborActions, LaborReducer, LaborActionCreator };
--- mazdoor_app_v2/store/reducers/labor/laborSlice.ts
+++ mazdoor_app_v2/store/reducers/labor/laborSlice.ts
@@ -2,38 +2,10 @@
 import { Labor as LaborInterface, Admin } from "@/models/interfaces";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface LaborCategory {
-  name: string;
-  nameKey: string;
-  description: string;
-  descriptionKey: string;
-  icon: string;
-  color: string;
-  laborCount: number;
-}
-
-interface Laborer {
-  id: string;
-  name: string;
-  category: string;
-  image: string;
-  age: number;
-  cnic: string;
-  area: string;
-  phone: string;
-  rating: number;
-  experience: string;
-  hourlyRate: number;
-  isAvailable: boolean;
-  skills: string[];
-}
-
 interface Labor {
   team: Admin[];
   labors: LaborInterface[];
   laborTypes: string[];
-  laborCategories: LaborCategory[];
-  transformedLaborers: Laborer[];
   isLoading: boolean;
   error: string | null;
 }
@@ -42,8 +14,6 @@ const initialState: Labor = {
   team: [],
   labors: [],
   laborTypes: [],
-  laborCategories: [],
-  transformedLaborers: [],
   isLoading: false,
   error: null,
 };
@@ -60,12 +30,6 @@ const laborSlice = createSlice({
     },
     setLaborTypes: (state, action: PayloadAction<string[]>) => {
       state.laborTypes = action.payload;
-    },
-    setLaborCategories: (state, action: PayloadAction<LaborCategory[]>) => {
-      state.laborCategories = action.payload;
-    },
-    setTransformedLaborers: (state, action: PayloadAction<Laborer[]>) => {
-      state.transformedLaborers = action.payload;
     },
     setLoading: (state, action: PayloadAction<boolean>) => {
       state.isLoading = action.payload;
